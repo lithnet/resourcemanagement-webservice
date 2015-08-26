601a10f46c14a2455bacdb46b53017c18b300687
ryannewington <ryan@lithiumblue.com>
2015-08-26 16:13:40 +1000

    Added WebException handlers to all methods

--- ResourceManagementWebServicev1.svc.cs
+++ ResourceManagementWebServicev1.svc.cs
@@ -20,30 +20,45 @@ namespace Lithnet.ResourceManagement.WebService
     {
         public IEnumerable<ResourceObject> GetResources()
         {
-            string attributes = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["attributes"];
-            string objectType = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["objectType"];
-            string filter = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["filter"];
-
-            if (filter == null && objectType == null)
+            try
             {
-                filter = "/*";
-            }
-            else if (filter == null && objectType != null)
-            {
-                filter = string.Format("/{0}", objectType);
-            }
+                string attributes = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["attributes"];
+                string objectType = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["objectType"];
+                string filter = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["filter"];
 
-            if (attributes != null)
-            {
-                return Global.Client.GetResources(filter, attributes.Split(','));
-            }
+                if (filter == null && objectType == null)
+                {
+                    filter = "/*";
+                }
+                else if (filter == null && objectType != null)
+                {
+                    filter = string.Format("/{0}", objectType);
+                }
 
-            if (objectType != null)
-            {
-                return Global.Client.GetResources(filter, ResourceManagementSchema.ObjectTypes[objectType].Attributes.Select(t => t.SystemName));
-            }
+                if (attributes != null)
+                {
+                    return Global.Client.GetResources(filter, attributes.Split(','));
+                }
 
-            return Global.Client.GetResources(filter);
+                if (objectType != null)
+                {
+                    return Global.Client.GetResources(filter, ResourceManagementSchema.ObjectTypes[objectType].Attributes.Select(t => t.SystemName));
+                }
+
+                return Global.Client.GetResources(filter);
+            }
+            catch (WebFaultException)
+            {
+                throw;
+            }
+            catch (WebFaultException<ExceptionData>)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         public ResourceObject GetResourceByKey(string objectType, string key, string keyValue)
@@ -103,155 +118,248 @@ namespace Lithnet.ResourceManagement.WebService
 
         public KeyValuePair<string, string[]> GetResourceAttributeByID(string id, string attribute)
         {
-            ResourceObject resource = Global.Client.GetResource(id, new List<string>() { attribute });
-            object value = resource.Attributes[attribute].Value;
-            List<string> valuesToReturn = new List<string>();
-
-            if (value is string)
+            try
             {
-                valuesToReturn.Add(value as string);
-            }
-            else if (value is byte[])
-            {
-                valuesToReturn.Add(Convert.ToBase64String((byte[])value));
-            }
-            else
-            {
-                IEnumerable values = value as IEnumerable;
-                if (values != null)
+                ResourceObject resource = Global.Client.GetResource(id, new List<string>() { attribute });
+                if (resource == null)
                 {
-                    foreach (object enumvalue in values)
-                    {
-                        if (enumvalue is DateTime)
-                        {
-                            valuesToReturn.Add(((DateTime)enumvalue).ToResourceManagementServiceDateFormat());
-                        }
-                        else if (enumvalue is byte[])
-                        {
-                            valuesToReturn.Add(Convert.ToBase64String((byte[])enumvalue));
-                        }
-                        else
-                        {
-                            valuesToReturn.Add(enumvalue.ToString());
-                        }
-                    }
+                    throw new WebFaultException(System.Net.HttpStatusCode.NotFound);
+                }
+
+                object value = resource.Attributes[attribute].Value;
+                List<string> valuesToReturn = new List<string>();
+
+                if (value is string)
+                {
+                    valuesToReturn.Add(value as string);
+                }
+                else if (value is byte[])
+                {
+                    valuesToReturn.Add(Convert.ToBase64String((byte[])value));
                 }
                 else
                 {
-                    valuesToReturn.Add(value.ToString());
+                    IEnumerable values = value as IEnumerable;
+                    if (values != null)
+                    {
+                        foreach (object enumvalue in values)
+                        {
+                            if (enumvalue is DateTime)
+                            {
+                                valuesToReturn.Add(((DateTime)enumvalue).ToResourceManagementServiceDateFormat());
+                            }
+                            else if (enumvalue is byte[])
+                            {
+                                valuesToReturn.Add(Convert.ToBase64String((byte[])enumvalue));
+                            }
+                            else
+                            {
+                                valuesToReturn.Add(enumvalue.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        valuesToReturn.Add(value.ToString());
+                    }
                 }
-            }
 
-            return new KeyValuePair<string, string[]>(attribute, valuesToReturn.ToArray());
+                return new KeyValuePair<string, string[]>(attribute, valuesToReturn.ToArray());
+            }
+            catch (WebFaultException)
+            {
+                throw;
+            }
+            catch (WebFaultException<ExceptionData>)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         public KeyValuePair<string, string[]> GetResourceAttributeByKey(string objectType, string key, string keyValue, string attribute)
         {
-            ResourceObject resource = Global.Client.GetResourceByKey(objectType, key, keyValue, new List<string>() { attribute });
-            object value = resource.Attributes[attribute].Value;
-            List<string> valuesToReturn = new List<string>();
-
-            if (value is string)
+            try
             {
-                valuesToReturn.Add(value as string);
-            }
-            else if (value is byte[])
-            {
-                valuesToReturn.Add(Convert.ToBase64String((byte[])value));
-            }
-            else
-            {
-                IEnumerable values = value as IEnumerable;
-                if (values != null)
+                ResourceObject resource = Global.Client.GetResourceByKey(objectType, key, keyValue, new List<string>() { attribute });
+                if (resource == null)
                 {
-                    foreach (object enumvalue in values)
-                    {
-                        if (enumvalue is DateTime)
-                        {
-                            valuesToReturn.Add(((DateTime)enumvalue).ToResourceManagementServiceDateFormat());
-                        }
-                        else if (enumvalue is byte[])
-                        {
-                            valuesToReturn.Add(Convert.ToBase64String((byte[])enumvalue));
-                        }
-                        else
-                        {
-                            valuesToReturn.Add(enumvalue.ToString());
-                        }
-                    }
+                    throw new WebFaultException(System.Net.HttpStatusCode.NotFound);
+                }
+
+                object value = resource.Attributes[attribute].Value;
+                List<string> valuesToReturn = new List<string>();
+
+                if (value is string)
+                {
+                    valuesToReturn.Add(value as string);
+                }
+                else if (value is byte[])
+                {
+                    valuesToReturn.Add(Convert.ToBase64String((byte[])value));
                 }
                 else
                 {
-                    valuesToReturn.Add(value.ToString());
+                    IEnumerable values = value as IEnumerable;
+                    if (values != null)
+                    {
+                        foreach (object enumvalue in values)
+                        {
+                            if (enumvalue is DateTime)
+                            {
+                                valuesToReturn.Add(((DateTime)enumvalue).ToResourceManagementServiceDateFormat());
+                            }
+                            else if (enumvalue is byte[])
+                            {
+                                valuesToReturn.Add(Convert.ToBase64String((byte[])enumvalue));
+                            }
+                            else
+                            {
+                                valuesToReturn.Add(enumvalue.ToString());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        valuesToReturn.Add(value.ToString());
+                    }
                 }
-            }
 
-            return new KeyValuePair<string, string[]>(attribute, valuesToReturn.ToArray());
+                return new KeyValuePair<string, string[]>(attribute, valuesToReturn.ToArray());
+            }
+            catch (WebFaultException)
+            {
+                throw;
+            }
+            catch (WebFaultException<ExceptionData>)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         public void DeleteResourceByID(string id)
         {
-            Global.Client.DeleteResource(id);
+            try
+            {
+                Global.Client.DeleteResource(id);
+            }
+            catch (ResourceNotFoundException)
+            {
+                throw new WebFaultException(System.Net.HttpStatusCode.NotFound);
+            }
+            catch (WebFaultException)
+            {
+                throw;
+            }
+            catch (WebFaultException<ExceptionData>)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         public string CreateResource(ResourceUpdateRequest request)
         {
-            AttributeValueUpdate objectTypeUpdate = request.Attributes.FirstOrDefault(t => t.Name == AttributeNames.ObjectType);
-
-            if (objectTypeUpdate == null)
+            try
             {
-                throw new ArgumentException("An object type must be specified");
+                AttributeValueUpdate objectTypeUpdate = request.Attributes.FirstOrDefault(t => t.Name == AttributeNames.ObjectType);
+
+                if (objectTypeUpdate == null)
+                {
+                    throw new ArgumentException("An object type must be specified");
+                }
+
+                string objectType = objectTypeUpdate.Value == null ? null : objectTypeUpdate.Value[0];
+
+                if (objectType == null)
+                {
+                    throw new ArgumentException("An object type must be specified");
+                }
+
+                ResourceObject resource = Global.Client.CreateResource(objectType);
+
+                foreach (AttributeValueUpdate kvp in request.Attributes)
+                {
+                    if (kvp.Value.Length > 1)
+                    {
+                        resource.Attributes[kvp.Name].SetValue(kvp.Value);
+                    }
+                    else if (kvp.Value.Length == 1)
+                    {
+                        resource.Attributes[kvp.Name].SetValue(kvp.Value[0]);
+                    }
+                    else
+                    {
+                        resource.Attributes[kvp.Name].RemoveValues();
+                    }
+                }
+
+                Global.Client.SaveResource(resource);
+                return resource.ObjectID.ToString(false);
             }
-
-            string objectType = objectTypeUpdate.Value == null ? null : objectTypeUpdate.Value[0];
-
-            if (objectType == null)
+            catch (WebFaultException)
             {
-                throw new ArgumentException("An object type must be specified");
+                throw;
             }
-
-            ResourceObject resource = Global.Client.CreateResource(objectType);
-
-            foreach (AttributeValueUpdate kvp in request.Attributes)
+            catch (WebFaultException<ExceptionData>)
             {
-                if (kvp.Value.Length > 1)
-                {
-                    resource.Attributes[kvp.Name].SetValue(kvp.Value);
-                }
-                else if (kvp.Value.Length == 1)
-                {
-                    resource.Attributes[kvp.Name].SetValue(kvp.Value[0]);
-                }
-                else
-                {
-                    resource.Attributes[kvp.Name].RemoveValues();
-                }
+                throw;
             }
-
-            Global.Client.SaveResource(resource);
-            return resource.ObjectID.ToString(false);
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
 
         public void UpdateResource(string id, ResourceUpdateRequest request)
         {
-            ResourceObject resource = Global.Client.GetResource(id);
-            foreach (AttributeValueUpdate kvp in request.Attributes)
+            try
             {
-                if (kvp.Value.Length > 1)
+                ResourceObject resource = Global.Client.GetResource(id);
+                foreach (AttributeValueUpdate kvp in request.Attributes)
                 {
-                    resource.Attributes[kvp.Name].SetValue(kvp.Value);
+                    if (kvp.Value.Length > 1)
+                    {
+                        resource.Attributes[kvp.Name].SetValue(kvp.Value);
+                    }
+                    else if (kvp.Value.Length == 1)
+                    {
+                        resource.Attributes[kvp.Name].SetValue(kvp.Value[0]);
+                    }
+                    else
+                    {
+                        resource.Attributes[kvp.Name].RemoveValues();
+                    }
                 }
-                else if (kvp.Value.Length == 1)
-                {
-                    resource.Attributes[kvp.Name].SetValue(kvp.Value[0]);
-                }
-                else
-                {
-                    resource.Attributes[kvp.Name].RemoveValues();
-                }
-            }
 
-            Global.Client.SaveResource(resource);
+                Global.Client.SaveResource(resource);
+            }
+            catch (ResourceNotFoundException)
+            {
+                throw new WebFaultException(System.Net.HttpStatusCode.NotFound);
+            }
+            catch (WebFaultException)
+            {
+                throw;
+            }
+            catch (WebFaultException<ExceptionData>)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw WebExceptionHelper.CreateWebException(HttpStatusCode.InternalServerError, ex);
+            }
         }
     }
 }
