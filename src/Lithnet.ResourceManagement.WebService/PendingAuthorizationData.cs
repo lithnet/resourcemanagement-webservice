b2a8c43d34a81d4693a41895ebccb6921e65452a
ryannewington <ryan@lithiumblue.com>
2016-09-01 11:29:41 +1000

    Adds support for returning a 202 (Accepted) response when authorization required is returned from the FIM service

---
+++ src/Lithnet.ResourceManagement.WebService/PendingAuthorizationData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using Lithnet.ResourceManagement.Client;
+
+namespace Lithnet.ResourceManagement.WebService
+{
+    [DataContract]
+    public class PendingAuthorizationData
+    {
+        public PendingAuthorizationData(AuthorizationRequiredException ex)
+        {
+            this.RequestID = ex.ResourceReference;
+            this.Endpoint = ex.Endpoint;
+            this.Message = "Authorization Pending";
+        }
+
+        [DataMember]
+        public string Message { get; set; }
+
+
+        [DataMember]
+        public string RequestID { get; set; }
+
+        [DataMember]
+        public string Endpoint { get; set; }
+    }
+}
